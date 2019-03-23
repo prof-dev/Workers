4ea82b1a9987e3782ffcaded7bbb5031a17e9353
mohamedmelsayed <mohamedosmanelsayed@yahoo.com>
2019-03-23 08:53:40 +0200

    pojos added

---
+++ app/src/main/java/com/example/workers/pojos/ShopExpenses.java
@@ -0,0 +1,4 @@
+package com.example.workers.pojos;
+
+public class ShopExpenses {
+}
---
+++ app/src/main/java/com/example/workers/pojos/Shops.java
@@ -0,0 +1,11 @@
+package com.example.workers.pojos;
+import java.util.Date;
+public class Shops {
+
+    private int id;
+    private String name;
+    private String address;
+    private int manager_id;
+    private int is_active;
+    private Date activation_date;
+}
